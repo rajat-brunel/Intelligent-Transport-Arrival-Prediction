e4c5f1ceececbc449449217aced5ea1eabca9bd9
rajat <r.awa3124@gmail.com>
2019-02-07 15:45:20 +0000

    Changes to dataset header

--- Visualisation/dataset_difference.py
+++ Visualisation/dataset_difference.py
@@ -8,18 +8,14 @@ sns.set()
 
 data = pd.read_csv('../Dataset/dataset.csv', parse_dates=True)
 
-data['arrival_time_at_last_Stop'] = pd.to_datetime(data['arrival_time_at_last_Stop'], format='%Y-%m-%dT%H:%M:%S.%f')
+data['time_at_origin'] = pd.to_datetime(data['time_at_origin'], format='%Y-%m-%dT%H:%M:%S.%f')
 data['arrival_time_at_next_Stop'] = pd.to_datetime(data['arrival_time_at_next_Stop'], format='%Y-%m-%dT%H:%M:%S.%f')
 
 
-data['diff'] = (data['arrival_time_at_next_Stop']-data['arrival_time_at_last_Stop'])/np.timedelta64(1, 's')
-data.set_index(pd.DatetimeIndex(data['arrival_time_at_last_Stop']), inplace=True)
+data['diff'] = (data['arrival_time_at_next_Stop']-data['time_at_origin'])/np.timedelta64(1, 's')
+data.set_index(pd.DatetimeIndex(data['time_at_origin']), inplace=True)
 
 data = data[data['mode'] == "bus"]
 
 print(data['vehicle_id'].value_counts())
 
-plt.figure(figsize=(12, 8))
-
-
-plt.show()
